b967307da3fb753cec4ec40ef5381b26c00a9834
Mikhail Yakshin <greycat@altlinux.org>
2012-10-26 04:22:02 +0400

    Added JavaScript-driven column selection state persistence, stored in localStorage

--- js/comparison.js
+++ js/comparison.js
@@ -1,9 +1,49 @@
-function switchColumn(el, col) {
+function saveSelected() {
+	var compared = [];
+
 	var tbl = document.getElementById('comparison');
-	var state = el.checked ? '' : 'none';
+	var tblHead = tbl.rows[0].cells;
+	for (var i = 1; i < tblHead.length; i++) {
+		var lang = tblHead[i].id.substring(7);
+		if (tblHead[i].style.display != 'none') {
+			compared.push(lang);
+		}
+	}
+
+	localStorage.setItem('compared', compared.join(','));
+}
+
+function loadSelected() {
+	var comparedStr = localStorage.getItem('compared');
+	if (comparedStr == null)
+		return;
+
+	var compared = comparedStr.split(',');
+
+	var tbl = document.getElementById('comparison');
+	var tblHead = tbl.rows[0].cells;
+	for (var i = 1; i < tblHead.length; i++) {
+		var lang = tblHead[i].id.substring(7);
+		var state = compared.indexOf(lang) < 0 ? false : true
+		setColumnVisibility(tbl, i, state);
+		document.getElementById('check-' + lang).checked = state;
+	}
+}
+
+function switchColumn(el, col) {
+	setColumnVisibility(
+		document.getElementById('comparison'),
+		col,
+		el.checked
+	);
+	saveSelected();
+}
+
+function setColumnVisibility(tbl, col, state) {
+	var d = state ? '' : 'none';
 	for (var i = 0; i < tbl.rows.length; i++) {
 		var cell = tbl.rows[i].cells[col];
 		if (cell != null)
-			cell.style.display = state;
+			cell.style.display = d;
 	}
 }
