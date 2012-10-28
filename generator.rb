67e2d808d5986e24edbe64682cdb4d091a6e3316
Mikhail Yakshin <greycat@altlinux.org>
2012-10-29 02:04:09 +0400

    Added directory contents validity check; extra trash files would be frowned upon - hope it would help to reduce file naming errors

--- generator.rb
+++ generator.rb
@@ -48,6 +48,7 @@ class Generator
 
 	def recurse_dir(depth, dir)
 		Dir.glob("#{dir}/*").sort.each { |d|
+			check_validity(d) unless depth == 1
 			next unless FileTest.directory?(d)
 			process_dir(depth, d)
 			recurse_dir(depth + 1, d)
@@ -84,5 +85,48 @@ class Generator
 			}
 			@full_page.row(desc, cols)
 		end
+	end
+
+	class ParseException < Exception; end
+
+	KINDS = [
+		'ref',
+		'long',
+	]
+
+	# Checks that directory entry is valid and we know how to
+	# interprete it. If we don't, raises a parsing exception.
+	def check_validity(fn)
+		# Any directory is valid - we'll just recurse into it.
+		# We're only checking file names.
+		return if FileTest.directory?(fn)
+
+		n = File.basename(fn)
+
+		topic = kind = lang = nil
+
+		case n
+		when /^([^-]+)$/
+			topic = $1
+		when /^([^-]+)-([^-]+)$/
+			topic = $1
+			if KINDS.include?($2)
+				kind = $2
+			else
+				lang = $2
+			end
+		when /^([^-]+)-([^-]+)-([^-]+)$/
+			topic, kind, lang = $1, $2, $3
+		end
+
+		# Just a topic name: normal table data entry, good
+		# for all languages
+		raise ParseException.new("Unknown topic '#{topic}' encountered in file '#{fn}'") unless @topic_names[topic] or topic == 'desc'
+
+		# Language must be 2-letter code, if present
+		raise ParseException.new("Bad language code '#{lang}' encountered in file '#{fn}'") unless lang.nil? or lang =~ /^[a-z]{2}$/
+
+		# Kind must be a valid kind
+		raise ParseException.new("Invalid kind of file '#{kind}' encountered in file '#{fn}'") unless kind.nil? or KINDS.include?(kind)
 	end
 end
