dad8040d83ec728cafe7b9b18ff535eac1470275
Mikhail Yakshin <greycat@altlinux.org>
2012-11-03 03:07:10 +0400

    Refactored HTMLHelper, added render_and_append method

--- htmlhelper.rb
+++ htmlhelper.rb
@@ -2,10 +2,15 @@ require 'erb'
 
 module HTMLHelper
 	def render_and_output(tmpl_file, data, out_file)
-		out_path = File.join(@opt[:out], out_file)
-
 		# TODO: mkdir all components of path
-		File.open(out_path, 'w') { |f|
+		File.open(out_path(out_file), 'w') { |f|
+			f << render(tmpl_file, data)
+		}
+	end
+
+	def render_and_append(tmpl_file, data, out_file)
+		# TODO: mkdir all components of path
+		File.open(out_path(out_file), 'a') { |f|
 			f << render(tmpl_file, data)
 		}
 	end
@@ -13,5 +18,10 @@ module HTMLHelper
 	def render(tmpl_file, data)
 		tmpl = File.open(File.join(@opt[:style], tmpl_file)).read
 		ERB.new(tmpl, nil, nil, "_e#{tmpl_file.hash.abs}").result(data)
+	end
+
+	private
+	def out_path(out_file)
+		File.join(@opt[:out], out_file)
 	end
 end
